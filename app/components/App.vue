a4330eb6fea84dba438fc7570a7d8df32b98d740
松村和輝 <k.matsumura0314@gmail.com>
2018-03-15 21:23:27 +0900

    first commit

---
+++ app/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{hoge}}</h1>
+</template>
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      hoge: 'fuga',
+    };
+  }
+});
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, './app/dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          }
+        }
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devtool: '#eval-source-map'
+}
